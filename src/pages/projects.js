4f2da34f45c75284823613b180e6adac7bf2783c
Enrique <erivera7240@gmail.com>
2024-01-30 11:34:23 -0600

    first project update

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import proj1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import proj1 from "../../public/images/projects/NextJS-Ecom-Cover-Image.png";
 import proj2 from "../../public/images/projects/nft-collection-website-cover-image.jpg";
 import proj3 from "../../public/images/projects/fashion-studio-website.jpg";
 import proj4 from "../../public/images/projects/portfolio-cover-image.jpg";
@@ -182,19 +182,21 @@ export default function Projects() {
           />
         <h2 className="my-2 w-full text-left text-4xl font-bold lg:text-3xl xs:text-2xl">
             Coming Soon...!
-          </h2>
-          {/* <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+        </h2>
+
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
                 type="Featured Project"
-                title="Crypto Screener Application"
-                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."
+                title="Next.Js Ecommerce Storefront"
+                summary="An agile Next.js Typescript application that uses Shopify’s storefront API or other headless CMS API to
+                build an Ecommerce quickly and reliably."
                 img={proj1}
-                link="https://devdreaming.com/videos/build-crypto-screener-app-with-react-tailwind-css"
-                github="https://github.com/codebucks27/CryptoBucks-Final-Code"
+                link=""
+                github="https://github.com/Quantum-Impulse/ecom-nextjs"
               />
             </div>
-            <div className="col-span-6 sm:col-span-12">
+            {/* <div className="col-span-6 sm:col-span-12">
               <Project
                 type="Website Template"
                 title="NFT collection Website"
@@ -239,8 +241,8 @@ export default function Projects() {
                 link="https://devdreaming.com"
                 github="https://github.com/codebucks27"
               />
-            </div>
-          </div> */}
+            </div> */}
+          </div>
         </Layout>
       </main>
     </>
